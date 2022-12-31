9a323d771ba391d123f96848dcf94ff7a3a3b420
GunPKate <gun@builk.com>
2022-12-31 22:13:29 +0700

    delete unused

--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
+	// "net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -35,108 +35,12 @@ func dbConnection() {
 	}
 }
 
-func serverHealthHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, "OK")
-}
 
-type User2 struct {
-	ID   int
-	AGE  int
-	NAME string
-}
-
-type Err2 struct {
-	Message string `json:"message"`
-}
-
-func createServerUsersHandler(c echo.Context) error {
-	var u User2
-	err := c.Bind(&u)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err2{Message: err.Error()})
-	}
-
-	row := db.QueryRow("INSERT INTO users (name,age) VALUES($1,$2) RETURNING id", u.NAME, u.AGE)
-	err = row.Scan(&u.ID) //add ID into u
-	if err != nil {
-		log.Fatal("can't insert data", err)
-	}
-
-	return c.JSON(http.StatusOK, u)
-}
-
-func getServerUsersHandler(c echo.Context) error {
-
-	stmt, err := db.Prepare("SELECT * from users")
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err2{Message: "can't prepare query all users statement"})
-	}
-
-	rows, err := stmt.Query()
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err2{Message: "can't query all users:" + err.Error()})
-	}
-
-	users := []User2{}
-	for rows.Next() {
-		var u User2
-		err = rows.Scan(&u.ID, &u.NAME, &u.AGE)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, Err2{Message: "can't query all users:" + err.Error()})
-		}
-		users = append(users, u)
-	}
-	return c.JSON(http.StatusOK, users)
-}
-
-func getServerUsersByIDHandler(c echo.Context) error {
-	id := c.Param("id")
-	stmt, err := db.Prepare("SELECT * from users WHERE id = $1")
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err2{Message: "can't prepare query all users statement"})
-	}
-
-	rows, err := stmt.Query(id)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err2{Message: "can't query all users:" + err.Error()})
-	}
-
-	users := []User2{}
-	for rows.Next() {
-		var u User2
-		err = rows.Scan(&u.ID, &u.NAME, &u.AGE)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, Err2{Message: "can't query all users:" + err.Error()})
-		}
-		users = append(users, u)
-	}
-	return c.JSON(http.StatusOK, users)
-}
-
-//shadowing
-// var db *sql.DB  	new db1
-// db, err : =		new db2
 
 var db *sql.DB
 
-// db, err = sql.Open("postgres", os.Getenv(("DATABASE_URL")))
-// DATABASE_URL = postgres://mkzchuoq:loPAe5lWPs4gsdvrMf2aKchys2xsGF0x@tiny.db.elephantsql.com/mkzchuoq
 func main() {
-	// var err error
-	// var url string = "postgres://mkzchuoq:loPAe5lWPs4gsdvrMf2aKchys2xsGF0x@tiny.db.elephantsql.com/mkzchuoq"
-	// // db, err = sql.Open("postgres", os.Getenv(("DATABASE_URL")))
-	// db, err = sql.Open("postgres", url)
-	// if err != nil {
-	// 	log.Fatal("Connection error", err)
-	// }
-	// fmt.Println("Server running")
-	// defer db.Close()
 
-	// createTb := `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT,age INT);`
-	// _, err = db.Exec(createTb)
-	// if err != nil {
-	// 	log.Fatal("can't create table", err)
-	// }
 	dbConnection()
 
 	e := echo.New()
@@ -144,10 +48,10 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/health", serverHealthHandler)
-	e.GET("/users", getServerUsersHandler)
-	e.GET("/users/:id", getServerUsersByIDHandler)
-	e.POST("/users", createServerUsersHandler)
+	// e.GET("/health", serverHealthHandler)
+	// e.GET("/users", getServerUsersHandler)
+	// e.GET("/users/:id", getServerUsersByIDHandler)
+	// e.POST("/users", createServerUsersHandler)
 
 	log.Println("Server start at: 2565")
 	log.Fatal(e.Start(":2565"))
