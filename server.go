4b3a5af4ce5658321dcb1954d03f177932a6840d
GunPKate <gun@builk.com>
2023-01-01 13:07:46 +0700

    get all expenses

--- server.go
+++ server.go
@@ -4,14 +4,65 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	// "net/http"
+
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
-func dbConnection() {
+var db *sql.DB
+
+// func dbConnection() {
+
+// }
+
+type Expense struct {
+	// id     string  `json:"id"`
+	// title  string  `json:"title"`
+	// amount float64 `json:"amount"`
+	// note   string  `json:"note"`
+	// tags   string  `json:"tags"`
+	id     string
+	title  string
+	amount float64
+	note   string
+	tags   pq.StringArray
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+func getAllExpensesHandler(c echo.Context) error {
+
+	stmt, err := db.Prepare("SELECT * from expenses")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expenses statement"})
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expenses:" + err.Error()})
+	}
+
+	expenses := []Expense{}
+	for rows.Next() {
+		var ex Expense
+		err = rows.Scan(&ex.id, &ex.title, &ex.amount, &ex.note, &ex.tags)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expenses:" + err.Error()})
+		}
+		expenses = append(expenses, ex)
+	}
+	return c.JSON(http.StatusOK, expenses)
+}
+
+func main() {
+
+	// dbConnection()
 	var err error
 	var url string = "postgres://mkzchuoq:loPAe5lWPs4gsdvrMf2aKchys2xsGF0x@tiny.db.elephantsql.com/mkzchuoq"
 	// db, err = sql.Open("postgres", os.Getenv(("DATABASE_URL")))
@@ -33,25 +84,13 @@ func dbConnection() {
 	if err != nil {
 		log.Fatal("can't create table", err)
 	}
-}
-
-
-
-var db *sql.DB
-
-func main() {
-
-	dbConnection()
 
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// e.GET("/health", serverHealthHandler)
-	// e.GET("/users", getServerUsersHandler)
-	// e.GET("/users/:id", getServerUsersByIDHandler)
-	// e.POST("/users", createServerUsersHandler)
+	e.GET("/expenses", getAllExpensesHandler)
 
 	log.Println("Server start at: 2565")
 	log.Fatal(e.Start(":2565"))
