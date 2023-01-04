382b3104ae712d2c6610a69990ff76dc6eeec3cc
GunPKate <gun@builk.com>
2023-01-04 21:44:01 +0700

    extract function dbconection and set handler type and struct

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	"net/http"
@@ -13,11 +12,38 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
+type handler struct {
+	DB *sql.DB
+}
 
-// func dbConnection() {
+func dbConnection(db *sql.DB) *handler {
+	return &handler{db}
+}
 
-// }
+func initConnection() *sql.DB {
+	// connStr := os.Getenv("DATABASE_URL")
+	connStr := "postgres://mkzchuoq:loPAe5lWPs4gsdvrMf2aKchys2xsGF0x@tiny.db.elephantsql.com/mkzchuoq"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	return db
+}
 
 type Expense struct {
 	Id     string         `json:"id"`
@@ -31,10 +57,10 @@ type Err struct {
 	Message string `json:"message"`
 }
 
-func getExpensesByIDHandler(c echo.Context) error {
+func (h *handler) getExpensesByIDHandler(c echo.Context) error {
 	id := c.Param("id")
 	var ex Expense
-	row := db.QueryRow("SELECT * from expenses WHERE id = $1", id)
+	row := h.DB.QueryRow("SELECT * from expenses WHERE id = $1", id)
 
 	err := row.Scan(&ex.Id, &ex.Title, &ex.Amount, &ex.Note, &ex.Tags)
 	if err != nil {
@@ -44,14 +70,14 @@ func getExpensesByIDHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, ex)
 }
 
-func postExpensesHandler(c echo.Context) error {
+func (h *handler) postExpensesHandler(c echo.Context) error {
 	var ex Expense
 	err := c.Bind(&ex)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
 	}
 
-	row := db.QueryRow("INSERT INTO expenses (title,amount,note,tags) VALUES($1,$2,$3,$4) RETURNING id",
+	row := h.DB.QueryRow("INSERT INTO expenses (title,amount,note,tags) VALUES($1,$2,$3,$4) RETURNING id",
 		ex.Title,
 		ex.Amount,
 		ex.Note,
@@ -65,9 +91,9 @@ func postExpensesHandler(c echo.Context) error {
 	return c.JSON(http.StatusCreated, ex)
 }
 
-func getAllExpensesHandler(c echo.Context) error {
+func (h *handler) getAllExpensesHandler(c echo.Context) error {
 
-	stmt, err := db.Prepare("SELECT * from expenses")
+	stmt, err := h.DB.Prepare("SELECT * from expenses")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expenses statement"})
 	}
@@ -89,7 +115,7 @@ func getAllExpensesHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, expenses)
 }
 
-func updateExpensesHandler(c echo.Context) error {
+func (h *handler) updateExpensesHandler(c echo.Context) error {
 	var ex Expense
 
 	id := c.Param("id")
@@ -98,7 +124,7 @@ func updateExpensesHandler(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
 	}
 	ex.Id = id
-	stmt, err := db.Prepare("UPDATE expenses SET title=$2, amount=$3, note=$4, tags=$5 WHERE id=$1")
+	stmt, err := h.DB.Prepare("UPDATE expenses SET title=$2, amount=$3, note=$4, tags=$5 WHERE id=$1")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
 	}
@@ -112,38 +138,17 @@ func updateExpensesHandler(c echo.Context) error {
 
 func main() {
 
-	// dbConnection()
-	var err error
-	var url string = "postgres://mkzchuoq:loPAe5lWPs4gsdvrMf2aKchys2xsGF0x@tiny.db.elephantsql.com/mkzchuoq"
-	// db, err = sql.Open("postgres", os.Getenv(("DATABASE_URL")))
-	db, err = sql.Open("postgres", url)
-	if err != nil {
-		log.Fatal("Connection error", err)
-	}
-	fmt.Println("Server running")
-	defer db.Close()
-
-	createTb := `CREATE TABLE IF NOT EXISTS expenses (
-		id SERIAL PRIMARY KEY,
-		title TEXT,
-		amount FLOAT,
-		note TEXT,
-		tags TEXT[]
-	);`
-	_, err = db.Exec(createTb)
-	if err != nil {
-		log.Fatal("can't create table", err)
-	}
+	db := initConnection()
 
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/expenses/:id", getExpensesByIDHandler)
-	e.POST("/expenses", postExpensesHandler)
-	e.GET("/expenses", getAllExpensesHandler)
-	e.PUT("/expenses/:id", updateExpensesHandler)
+	h := dbConnection(db)
+	e.GET("/expenses/:id", h.getExpensesByIDHandler)
+	e.POST("/expenses", h.postExpensesHandler)
+	e.GET("/expenses", h.getAllExpensesHandler)
+	e.PUT("/expenses/:id", h.updateExpensesHandler)
 
 	log.Println("Server start at: 2565")
 	log.Fatal(e.Start(":2565"))
